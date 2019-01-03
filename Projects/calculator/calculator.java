edb92b9c12e85e864475e0775c1690c1eefa1b53
torrapipes <crbergas@gmail.com>
2019-01-03 23:34:53 +0100

    added modulo method

--- Projects/calculator/calculator.java
+++ Projects/calculator/calculator.java
@@ -1,28 +1,45 @@
 class Calculator {
+  
     public Calculator {
       
     }
+    
     public int add(int a, int b) {
       int sum = a + b;
       return sum;
       }
+    
     public int substract(int a, int b) {
      int substract = a - b;
      return substract;
     }
+    
     public int multiply(int a, int b) {
      int multiplication = a * b;
      return multiplication;
     }
+    
     public int divide(int a, int b) {
+      
       if (b == 0){
+        
         System.out.println("Error! Division by zero is not allowed.");
         return 0;
+        
       }
       else{
+        
         int division = a / b;
-        return division;
+          return division;
+        
       }
       
-      }
+     }
+    
+    public int modulo(int a, int b) {
+      
+      modulo = a % b;
+      return modulo;
+      
+    }
   }
