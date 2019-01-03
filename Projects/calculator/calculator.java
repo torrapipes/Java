ed0fe70af38c5f3fa06d4f08bac91292c2f6d182
torrapipes <crbergas@gmail.com>
2019-01-03 23:06:13 +0100

    added public method add

--- Projects/calculator/calculator.java
+++ Projects/calculator/calculator.java
@@ -1,5 +1,9 @@
 class Calculator {
-    public Calculator{
-        
+    public Calculator {
+      
     }
-}
+    public int add(int a, int b) {
+      sum = a + b;
+      return sum;
+      }
+  }
