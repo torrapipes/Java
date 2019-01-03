cf1539addc49bb99905cd4c9fd4da2300cd430ec
torrapipes <crbergas@gmail.com>
2019-01-03 23:42:08 +0100

    fixed modulo and added main method

--- Projects/calculator/calculator.java
+++ Projects/calculator/calculator.java
@@ -20,26 +20,31 @@ class Calculator {
     }
     
     public int divide(int a, int b) {
-      
       if (b == 0){
-        
         System.out.println("Error! Division by zero is not allowed.");
         return 0;
-        
       }
       else{
-        
         int division = a / b;
           return division;
-        
       }
-      
      }
     
     public int modulo(int a, int b) {
-      
+      if (b == 0){
+        System.out.println("Error! Division by zero is not allowed.");
+        return 0;
+      }
+      else{
       modulo = a % b;
       return modulo;
+      }
+    }
+    public static void main(String[] args){
+      Calculator myCalculator = new Calculator();
+      System.out.println(myCalculator.add(5,7));
+        System.out.println(myCalculator.substract(45,11));
+      
       
     }
   }
