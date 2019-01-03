a9a27dd39413bc652db4725513508aa48e160631
torrapipes <crbergas@gmail.com>
2019-01-03 23:52:50 +0100

    added explanation

--- Projects/calculator/calculator.java
+++ Projects/calculator/calculator.java
@@ -1,22 +1,24 @@
+// This programs works as a calculator for two parameters, it adds, substracts, divides and calculates the modulo.
+
 class Calculator {
   
-    public Calculator {
+    public Calculator() {
       
-    }
+    } 
     
     public int add(int a, int b) {
-      int sum = a + b;
-      return sum;
-      }
+        int sum = a + b;
+        return sum;
+    }
     
     public int substract(int a, int b) {
-     int substract = a - b;
-     return substract;
+        int substract = a - b;
+        return substract;
     }
     
     public int multiply(int a, int b) {
-     int multiplication = a * b;
-     return multiplication;
+        int multiplication = a * b;
+        return multiplication;
     }
     
     public int divide(int a, int b) {
@@ -26,25 +28,24 @@ class Calculator {
       }
       else{
         int division = a / b;
-          return division;
+        return division;
       }
      }
     
     public int modulo(int a, int b) {
       if (b == 0){
-        System.out.println("Error! Division by zero is not allowed.");
-        return 0;
+          System.out.println("Error! Division by zero is not allowed.");
+          return 0;
       }
       else{
-      modulo = a % b;
-      return modulo;
+          int modulo = a % b;
+          return modulo;
       }
     }
     public static void main(String[] args){
-      Calculator myCalculator = new Calculator();
-      System.out.println(myCalculator.add(5,7));
+        Calculator myCalculator = new Calculator();
+        System.out.println(myCalculator.add(5,7));
         System.out.println(myCalculator.substract(45,11));
-      
       
     }
   }
