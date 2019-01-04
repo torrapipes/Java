97fb24f70a994e2da334ceb45f5689cd45aac7b8
torrapipes <crbergas@gmail.com>
2019-01-04 01:33:56 +0100

    added chargeBattery method

--- Projects/myDroid/myDroid.java
+++ Projects/myDroid/myDroid.java
@@ -14,4 +14,19 @@ public class Droid {
       System.out.println("Battery decreased to " + batteryLevel + "%");
       
     }
+
+    public void chargeBattery(int hours) {
+    
+        batteryLevel = batteryLevel + hours; 
+        if (batteryLevel > 100) {
+            batteryLevel = 100;
+      	    System.out.println("Battery Level is " + batteryLevel + "% Droid fully charged. Please disconnect.");
+        }
+        else {
+          
+          System.out.println("Droid is charging...");
+          System.out.println("Battery Level is " + batteryLevel + "%");
+          
+        }
+    }
 }
