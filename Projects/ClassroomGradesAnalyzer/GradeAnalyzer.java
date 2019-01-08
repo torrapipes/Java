c9b41f369cb0753333c255447e0bea00fe661896
torrapipes <crbergas@gmail.com>
2019-01-08 01:12:31 +0100

    created main method, ArrayList, object myAnalyzer  and called getAverage method

--- Projects/ClassroomGradesAnalyzer/GradeAnalyzer.java
+++ Projects/ClassroomGradesAnalyzer/GradeAnalyzer.java
@@ -34,5 +34,23 @@ public class GradeAnalyzer {
         }
         
       }
+
+      public static void main(String[] args)  {
+      
+        ArrayList<Integer> myClassroom = new ArrayList<Integer>();
+        
+        myClassroom.add(98);
+        myClassroom.add(92);
+        myClassroom.add(88);
+        myClassroom.add(75);
+        myClassroom.add(61);
+        myClassroom.add(89);
+        myClassroom.add(95);
+        
+        GradeAnalyzer myAnalyzer = new GradeAnalyzer();
+        
+        myAnalyzer.getAverage(myClassroom);
+        
+      }
   
 }
