77694e75dfde092ddae28c884824a6408b90f895
torrapipes <crbergas@gmail.com>
2019-01-08 01:42:13 +0100

    created lowest method

--- Projects/ClassroomGradesAnalyzer/GradeAnalyzer.java
+++ Projects/ClassroomGradesAnalyzer/GradeAnalyzer.java
@@ -11,6 +11,7 @@ public class GradeAnalyzer {
         if (grades.size() < 1) {
           
           System.out.println("Error! The ArrayList is empty.");
+
           return 0;
           
         }
@@ -35,6 +36,70 @@ public class GradeAnalyzer {
         
       }
 
+      public int getLowestGrade(ArrayList<Integer> grades) {
+      
+        int lowest = grades.get(0);
+        
+        if (grades.size() < 1) {
+          
+          System.out.println("Error! The ArrayList is empty.");
+          
+          return 0;
+          
+        }
+        
+        else {
+          
+          for (int i = 0; i < grades.size(); i++) {
+            
+            if (grades.get(i) < lowest) {
+              
+              lowest = grades.get(i);
+              
+            }
+           
+         }
+         
+         System.out.println(lowest);
+          
+        }
+        
+        return lowest;
+        
+      }
+
+      public int getHighestGrade(ArrayList<Integer> grades) {
+      
+        int highest = grades.get(0);
+        
+        if (grades.size() < 1) {
+          
+          System.out.println("Error! The ArrayList is empty.");
+          
+          return 0;
+          
+        }
+        
+        else {
+          
+          for (int i = 0; i < grades.size(); i++) {
+            
+            if (grades.get(i) > highest) {
+              
+              highest = grades.get(i);
+              
+            }
+           
+         }
+         
+         System.out.println(highest);
+          
+        }
+        
+        return highest;
+        
+      }
+
       public static void main(String[] args)  {
       
         ArrayList<Integer> myClassroom = new ArrayList<Integer>();
@@ -50,6 +115,8 @@ public class GradeAnalyzer {
         GradeAnalyzer myAnalyzer = new GradeAnalyzer();
         
         myAnalyzer.getAverage(myClassroom);
+
+        myAnalyzer.getLowestGrade(myClassroom);
         
       }
   
