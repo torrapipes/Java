fffc34810f0729dfec673e70fa63efc98d0103b2
torrapipes <crbergas@gmail.com>
2019-01-08 01:01:55 +0100

    created getAverage method

--- Projects/ClassroomGradesAnalyzer/GradeAnalyzer.java
+++ Projects/ClassroomGradesAnalyzer/GradeAnalyzer.java
@@ -6,4 +6,33 @@ public class GradeAnalyzer {
 
     }
 
+    public int getAverage(ArrayList<Integer> grades){
+      
+        if (grades.size() < 1) {
+          
+          System.out.println("Error! The ArrayList is empty.");
+          return 0;
+          
+        }
+        
+        else{
+          
+          int sum = 0;
+          
+          for (Integer grade : grades) {
+            
+            sum = sum + grade;
+            
+          }
+          
+          int average = sum / grades.size();
+          
+          System.out.println(average);
+          
+          return average;
+          
+        }
+        
+      }
+  
 }
