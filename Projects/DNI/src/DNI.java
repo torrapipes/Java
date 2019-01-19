9bd02452b89085ed2865dbf737d74220f3f906fe
torrapipes <crbergas@gmail.com>
2019-01-19 23:36:42 +0100

    modified dni

--- Projects/DNI/src/DNI.java
+++ Projects/DNI/src/DNI.java
@@ -1,11 +1,42 @@
 public class DNI {
 
     private String dni = null;
+    private TablaAsignacion tabla = new TablaAsignacion();
 
+    // Constructor
+    public DNI(String){
 
+        this.dni = dni;
+    }
 
-    public Dni(String){
+    // getters
+    public String getDni(){
+
+        return this.dni;
 
     }
+
+    public String getParteNumerica(String dni){
+
+        return this.parteNumerica;
+
+    }
+
+    public String getLetra(String dni){
+
+        return this.letra;
+
+    }
+
+    //setters
+    public void setParteNumerica(String dni){
+
+        this.
+
+    }
+
+    private calcularLetra(String)
+
+
 
 }
---
+++ Projects/DNI/src/TablaAsignacion.java
@@ -0,0 +1,15 @@
+
+
+public class TablaAsignacion {
+
+    char[] tabla = {'T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D', 'X', 'B', 'N', 'J', 'Z', 'S', 'Q', 'V', 'H', 'L',
+            'C', 'K', 'E' };
+
+
+    public calcularLetra(String){
+
+
+
+    }
+
+}
