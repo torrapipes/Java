d524515add3ccb2e4c9d7404c189bbd0d1e8b061
torrapipes <crbergas@gmail.com>
2019-01-20 00:22:14 +0100

    added getters and two methods

--- Projects/BiciPalma/src/Estacion/Estacion.java
+++ Projects/BiciPalma/src/Estacion/Estacion.java
@@ -15,6 +15,24 @@ public class Estacion {
 
     }
 
-    
+    public int getId() {
+        return this.id;
+    }
 
+    public String getDireccion() {
+        return this.direccion;
+    }
+
+    public int getNumeroAnclajes() {
+        return this.numeroAnclajes;
+    }
+
+    public String consultarEstacion() {
+        System.out.println("El id de la estación es " + this.id + ", la dirección es " + this.direccion + " y el número de anclajes es " + this.numeroAnclajes);
+    }
+
+    public int anclajesLibres() {
+        //int result = numeroAnclajes - anclajes.size();
+
+    }
 }
