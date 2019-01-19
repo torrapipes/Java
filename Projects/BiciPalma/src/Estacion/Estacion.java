4ca14abd43ef30b535e1a4a2d2eb020e4998fe75
torrapipes <crbergas@gmail.com>
2019-01-19 23:37:46 +0100

    started new project

---
+++ Projects/BiciPalma/src/Estacion/Estacion.java
@@ -0,0 +1,20 @@
+package Estacion;
+
+public class Estacion {
+
+    private int id;
+    String direccion;
+    int numeroAnclajes;
+    int[] anclajes[numeroAnclajes];
+
+    public Estacion(int id, String direccion, int numeroAnclajes) {
+
+        this.id = id;
+        this.direccion = direccion;
+        this.numeroAnclajes = numeroAnclajes;
+
+    }
+
+    
+
+}
