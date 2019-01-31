ca754f71cbf2be906b50fe4a7de519d3d55863f2
torrapipes <crbergas@gmail.com>
2019-01-31 02:22:54 +0100

    Test cases passed

--- Projects/bowlingGame/src/main/java/org/lasencinas/ScoreCard.java
+++ Projects/bowlingGame/src/main/java/org/lasencinas/ScoreCard.java
@@ -2,13 +2,83 @@ package org.lasencinas;
 
 public class ScoreCard {
 
-    public ScoreCard(String ScoreCard){
+    // Global variables
+    private int STRIKE = 10;
+    private int SPARE = 10;
+    private int ZERO = 0;
+    private String pinsSerie = "-123456789";
+    private String scoreCard = "";
+    private int scoreTotal = 0;
 
+
+    // Constructors
+    public ScoreCard() {
+    }
+
+    public ScoreCard(String scoreCard) {
+        this.scoreCard = scoreCard;
+    }
+
+
+    // Getters
+    public int getSTRIKE() {
+        return this.STRIKE;
+    }
+
+    public int getZERO() {
+        return this.ZERO;
+    }
+
+    public int getSPARE() {
+        return this.SPARE;
+    }
+
+
+    //Methods
+    public void computeStrike(String strike, int ball) {
+
+            scoreTotal += this.getSTRIKE();
+            scoreTotal += this.computePins(strike.charAt(ball + 1));
+            scoreTotal += this.computePins(strike.charAt(ball + 2));
 
     }
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+
+
+    public void computeSpare(String spare, int ball) {
+        try {
+            if (spare.charAt(ball) == '/') {
+                scoreTotal += this.getSPARE() - this.computePins(spare.charAt(ball - 1));
+            } else {
+                scoreTotal += this.getZERO();
+            }
+        } catch (StringIndexOutOfBoundsException e) {
+            System.out.println(0);
+        }
     }
 
+
+    public int computePins(char pins) {
+        return this.pinsSerie.indexOf(pins);
+    }
+
+
+    public int computeScoreCard(String scoreCard) {
+        for (int ball = 0; ball < scoreCard.length(); ball++) {
+            if (scoreCard.charAt(ball) == 'X') {
+                computeStrike(scoreCard, ball);
+            }
+            else if(scoreCard.charAt(ball) == '/') {
+                computeSpare(scoreCard, ball);
+            }
+            else
+             {
+                scoreTotal += this.computePins(scoreCard.charAt(ball));
+            }
+        }
+        return scoreTotal;
+    }
+
+    // Main Method
+    public static void main(String[] args) {
+    }
 }
--- Projects/bowlingGame/src/test/java/org/lasencinas/ScoreCardTest.java
+++ Projects/bowlingGame/src/test/java/org/lasencinas/ScoreCardTest.java
@@ -1,19 +1,64 @@
 package org.lasencinas;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
 
+import org.junit.Before;
 import org.junit.Test;
 
-/**
- * Unit test for simple ScoreCard.
- */
+
 public class ScoreCardTest {
 
+    private ScoreCard scoreCard = null;
+
+    @Before
+    public void init() {
+        scoreCard = new ScoreCard();
+    }
+
+
     @Test
-    public void computeStrikeTest() {
+    public void computePinsTest() {
+        String pins = "-123456789";
+        assertEquals(0, scoreCard.computePins('-'));
 
-        ScoreCard scoreCard = new ScoreCard();
+        for (Integer i = 0; i < pins.length(); i++) {
+            pins.charAt(i);
+        }
+    }
 
-        assertEquals((10), ScoreCard.computeStrike("X"));
+
+    @Test
+    public void computeScoreCardTest1(){
+        String twentyPins = "11111111111111111111";
+        scoreCard = new ScoreCard(twentyPins);
+
+        assertEquals(20, scoreCard.computeScoreCard(twentyPins));
+    }
+
+
+    @Test
+    public void computeScoreCardTest2(){
+        String regularPins = "-123456789";
+        scoreCard = new ScoreCard((regularPins));
+
+        assertEquals(45, scoreCard.computeScoreCard(regularPins));
+    }
+
+
+    @Test
+    public void computeScoreCardTest3() {
+        String strikePins = "X1234";
+        scoreCard = new ScoreCard(strikePins);
+
+        assertEquals(23, scoreCard.computeScoreCard(strikePins));
+    }
+
+
+    @Test
+    public void computeScoreCardTest4() {
+        String spare = "1/23";
+        scoreCard = new ScoreCard(spare);
+
+        assertEquals(15, scoreCard.computeScoreCard(spare));
     }
 }
